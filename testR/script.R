86241e4ace8e2fd48da4e581a9cdba03052d2b70
Maxim Chistov <m.chistov@fundcount.com>
2017-02-03 19:37:15 +0300

    some changes

--- testR/script.R
+++ testR/script.R
@@ -1,4 +1,4 @@
-﻿library(dplyr)
+library(dplyr)
 
 #грузим свечки
 eurusd <- read.csv("C:\\Users\\maxpo\\Desktop\\EURUSDb5.csv")
@@ -25,27 +25,36 @@ eurusd$Close <- eurusd$Close / 2
 eurusd$Volume <- eurusd$Volume / 1000 # это вообще наугад, максимум по имеющимся данным - 712
 
 # Вывод для проверки
-View(eurusd, "Нормализованные данные(0...1)")
+View(eurusd, "Normalized(0...1)")
 
 # итак, исходные данные в нужном формате, теперь надо сформировать их для обучения(на каждом шаге сеть смотрит на предлыдущие 5 свечей)
-row <- function(data, offset) {
-    return <- c(data[offset, 2:7], data[offset + 1, 2:7], data[offset + 2, 2:7], data[offset + 3, 2:7], data[offset + 4, 2:7]);
-}
-expected <- function(data, offset) {
-    return <- data[offset, 8]
-}
+row <- function(input, offset) {
+    return <- unname(c(offset, input[offset, 2:6], input[offset - 1, 2:6], input[offset - 2, 2:6], input[offset - 3, 2:6], input[offset - 4, 2:6]))
+    }
 
-total <- nrow(eurusd) - 1
+expected <- function(input2, offset) {
+     return <- unname(c(input2[offset, 7:7]))
+    }
 
-data <- data.frame(Open = numeric(total), High = numeric(total), Low(total) = numeric(total), Close(total) = numeric(total), Volume(total) = numeric(total), stringsAsFactors = FALSE)
-for (i in 1:total)
-    data[i]$Open = eurusd[i]$Open
-    data[i]$High = eurusd[i]$High
-    data[i]$Low = eurusd[i]$Low
-    data[i]$Close = eurusd[i]$Close
-    data[i]$Volume = eurusd[i]$Volume
+total <- nrow(eurusd)
+print("Total")
+print(total)
 
+data <- data.frame(
+   Open = numeric(),numeric(), numeric(), numeric(), numeric(), numeric(),
+   numeric(), numeric(), numeric(), numeric(), numeric(),
+   numeric(), numeric(), numeric(), numeric(), numeric(),
+   numeric(), numeric(), numeric(), numeric(), numeric(),
+   numeric(), numeric(), numeric(), numeric(), numeric(),
+    stringsAsFactors = FALSE)
+expectedAnswer <- data.frame(numeric(), stringsAsFactors = FALSE)
 
+for (i in 5:total) {
+    data <- rbind(data, row(eurusd, i))
+    expectedAnswer <- rbind(expectedAnswer, expected(eurusd, i))
+    }
+View(data)
+View(expectedAnswer)
 
-install.packages('neuralnet')
-library("neuralnet")
+#install.packages('neuralnet')
+#library("neuralnet")
